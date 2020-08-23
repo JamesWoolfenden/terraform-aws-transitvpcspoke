f86556d98ce23e575b049b5d99bf5e1e3e8d5e93
James Woolfenden <James.Woolfenden@gmail.com>
2020-08-23 16:02:07 +0100

    add any values

--- example/examplea/module.spoke.tf
+++ example/examplea/module.spoke.tf
@@ -1,8 +1,8 @@
 module "spoke" {
   source        = "../../"
-  bucket_name   = ""
+  bucket_name   = "somebucket"
   bucket_prefix = ""
-  config_file   = ""
+  config_file   = "file.json"
   log_level     = "INFO"
   common_tags   = { "createdby" = "Terraform" }
 }
