b572ac033c79a7cdc83b8b12cae25347042d7ded
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-29 14:16:30 +0100

    fix

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      version = "3.39.0"
+      version = "3.42.0"
       source  = "hashicorp/aws"
     }
 
     archive = {
       version = "2.0.0"
     }
-
   }
   required_version = ">=0.14.8"
 }
