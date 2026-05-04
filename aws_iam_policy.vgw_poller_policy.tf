f79f7407c0d47b75bdb6a65e5c196a018d195075
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:27:58 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- aws_iam_policy.vgw_poller_policy.tf
+++ aws_iam_policy.vgw_poller_policy.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "vgw_poller_role_policy" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   name   = var.policy_name
   path   = "/"
   policy = data.aws_iam_policy_document.vgw_poller_role_policy_document.json
--- data.aws_iam_policy_document.vgw_poller_policy_document.tf
+++ data.aws_iam_policy_document.vgw_poller_policy_document.tf
@@ -1,4 +1,6 @@
 data "aws_iam_policy_document" "vgw_poller_role_policy_document" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   #checkov:skip=CKV_AWS_111:
   #tfsec:ignore:AWS097:TODO
   statement {
